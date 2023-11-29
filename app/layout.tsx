0c5c1a1db48cb18370f0ed82945adfd9c4a19a4b
Bruno Pistarino <brunopistarino@gmail.com>
2023-11-29 00:36:40 -0300

    Update description in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   metadataBase: new URL("https://brunop.vercel.app"),
   title: "Bruno Pistarino",
-  description: "",
+  description:
+    "Estudio Ingeniería en Informática y me apasiona crear interfaces que resalten por su experiencia de usuario (UX). Soy una persona proactiva y emprendedora, siempre aprendiendo nuevas tecnologías y aplicándolas en proyectos personales.",
 };
 
 export default function RootLayout({
